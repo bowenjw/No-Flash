07581a6ac4eed3f5c8f608aef7c5decd4783115d
jbowen2 <jbowen2@users.noreply.github.com>
2020-03-03 10:57:23 -0500

    added in setting option

---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+{
+    type = "string-setting",
+    name = "SC-damage-hit-tint",
+    setting_type = "startup",
+    default_value = "off",
+    allowed_values = {"off", "white", "blue", "red", "green", "yellow"}
+}
+})
