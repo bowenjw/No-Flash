986dc53b16dda5ed36f7e9cf3f0c80f4e184391f
jbowen2 <jbowen2@users.noreply.github.com>
2020-03-03 10:57:01 -0500

    implmented tint change

--- data.lua
+++ data.lua
@@ -1,7 +1,22 @@
--- \base\prototypes\entity\demo-entities.lua
-data.raw["character"]["character"].damage_hit_tint = {
-    r = 0,
-    g = 0,
-    b = 0,
-    a = 0
-}
+local colors = {
+    off ={r = 0, g = 0, b = 0, a = 0},
+    white = {r = 1, g = 1, b = 1, a = 0},
+    blue = {r = 0, g = 0, b = 1, a = 0},
+    red = {r = 1, g = 0, b = 0, a = 0},
+    green = {r = 0, g = 1, b = 0, a = 0},
+    yellow = {r = 1, g = 0.8, b = 0, a = 0}
+  }
+local color_setting = settings.startup["SC-damage-hit-tint"].value
+if  color_setting == "off" then
+    data.raw["character"]["character"].damage_hit_tint = colors.off
+elseif color_setting == "white" then
+    data.raw["character"]["character"].damage_hit_tint = colors.white
+elseif color_setting == "blue" then
+    data.raw["character"]["character"].damage_hit_tint = colors.white
+elseif color_setting == "red" then
+    data.raw["character"]["character"].damage_hit_tint = colors.red
+elseif color_setting == "green" then
+    data.raw["character"]["character"].damage_hit_tint = colors.green
+elseif color_setting == "yellow" then
+    data.raw["character"]["character"].damage_hit_tint = colors.yellow
+end
